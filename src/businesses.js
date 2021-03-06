99e8066d960fb6e6d4d318c58fdfd420978f3420
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2021-03-06 17:11:05 -0500

    Added 13 businesses

---
+++ src/businesses.js
@@ -0,0 +1,54 @@
+export const businessesJSON = [
+  {
+    name: "Express Cuts 10.18",
+    link: "https://www.expresscuts1018.com/",
+  },
+  {
+    name: "Main Event Barbershop",
+    link: "https://maineventbarbershop.com/",
+  },
+  {
+    name: "Hair Me Out",
+    link: "http://www.hairmeout.com/",
+  },
+  {
+    name: "Unique Cuts",
+    link: "https://www.uniquecuts.net/",
+  },
+  {
+    name: "Seamless Hair Boutique",
+    link: "https://www.seamlesshairllc.com/",
+  },
+  {
+    name: "Logwood Company, LLC",
+    link: "https://www.ivyees.com/",
+  },
+  {
+    name: "Salon Monet",
+    link: "http://hairsalonmonet.com/",
+  },
+  {
+    name: "Society Barbershop",
+    link: "https://societybarbershop.business.site/",
+  },
+  {
+    name: "The Private Office Barbershop",
+    link: "https://tpobshop.com/",
+  },
+  {
+    name: "Styllistik",
+    link: "http://www.styllistik.com/",
+  },
+  {
+    name: "Crowned by Glory Studios",
+    link: "http://www.crownedbyglory.com/",
+  },
+  {
+    name: "Cramer's Hair studio",
+    link: "",
+  },
+  {
+    name: "Darcy's barbershop",
+    link: "",
+  },
+];

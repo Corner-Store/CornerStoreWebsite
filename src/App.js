356b2e18aee4c43f9071624435589d603e29a8f9
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2021-03-06 11:16:23 -0500

    Added business display information

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
 
+const businessesJSON = [
+  {
+    name: "Business 1",
+    link: "",
+  },
+  {
+    name: "Business 2",
+    link: "",
+  },
+];
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Corner Store</h1>
+      {businessesJSON.map((item, index) => (
+        <p>
+          <a href={item.link}>{item.name}</a>
+        </p>
+      ))}
     </div>
   );
 }

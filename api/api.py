8d77edf84f758c41a0b9f0412f68dbe76c00df54
hakroala <hakroala@bu.edu>
2021-03-13 11:23:42 -0500

    Added code to connect to postgres database

--- api/api.py
+++ api/api.py
@@ -1,10 +1,23 @@
 import time
 from flask import Flask
+import db
 
 #Temporary code: to be changed later
 
 app = Flask(__name__)
 
+
 @app.route('/time')
 def get_current_time():
 	return {'time': time.time()}
+
+
+@app.route('/businesses/names')
+def get_all_business_names():
+	try:
+		connection = db.get_connection()
+		names = db.get_all_business_names(connection)
+		return {'names': str(names)}
+	except Exception as e:
+		return {'error': str(e)}
+

6ab4b9a6f33645ea966d99472bc51e8a284e0545
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2021-03-27 09:37:28 -0400

    Update api.py

--- api/api.py
+++ api/api.py
@@ -14,7 +14,7 @@ app = Flask(__name__,static_folder='../../build', static_url_path='/')
 
 @app.route('/')
 def index():
-	return  app.send_static_file('../../build/index.html')
+	return  app.send_static_file('index.html')
 
 
 @app.route('/businesses')

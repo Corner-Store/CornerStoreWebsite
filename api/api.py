d34fa83ee356dbec7f8ed40d60af8e8978875697
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2021-03-27 09:28:37 -0400

    Update api.py

--- api/api.py
+++ api/api.py
@@ -14,7 +14,7 @@ app = Flask(__name__,static_folder='../../build', static_url_path='/')
 
 @app.route('/')
 def index():
-	return  app.send_static_file('index.html')
+	return  app.send_static_file('../../build/index.html')
 
 
 @app.route('/businesses')
@@ -38,7 +38,7 @@ def search():
 
 @app.errorhandler(404)
 def not_found(e):
-	return app.send_static_file('index.html')
+	return app.send_static_file('../../build/index.html')
 
 if __name__=="__main__":
     app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT',80))

0f52aaf56f120fea86b3b4e069d7ede2de9af8ae
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2021-03-27 10:00:12 -0400

    Update api.py

--- api/api.py
+++ api/api.py
@@ -4,7 +4,7 @@ import db
 import os
 
 
-app = Flask(__name__,static_folder='../../build', static_url_path='/')
+app = Flask(__name__,static_folder='../build', static_url_path='/')
 
 
 # Sets "NODE_ENV" to "production"
@@ -42,7 +42,7 @@ def favicon():
 
 @app.errorhandler(404)
 def not_found(e):
-	return app.send_static_file('../../build/index.html')
+	return app.send_static_file('index.html')
 
 if __name__=="__main__":
     app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT',80))

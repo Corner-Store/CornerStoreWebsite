d5aadef374d7469492dddd6f4d202c5da67febfe
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2021-03-27 01:04:01 -0400

    Update api.py

--- api/api.py
+++ api/api.py
@@ -7,9 +7,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def get_current_time():
-	return {'time': time.time()}
+# @app.route('/')
+# def get_current_time():
+# 	return {'time': time.time()}
 
 
 @app.route('/businesses')

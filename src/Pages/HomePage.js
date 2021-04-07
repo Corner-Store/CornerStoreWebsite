e9ddc1cdfad47eb083c754845d6429879cf03557
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2021-04-07 17:06:58 -0400

    Clean up code and added comments

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,47 +2,42 @@ import React, { Component } from "react";
 import Header from "../Components/Header";
 import SearchBusiness from "../Components/SearchBusinesses";
 import SearchCategories from "../Components/SearchCategories_Industries";
-import { withRouter, Redirect } from "react-router-dom";
+import { withRouter} from "react-router-dom";
 import "../layoutMain.css";
+
+//Description: The home page contains a search engine and different categories so that 
+//when an business or category is searched, the page will be directed to the search results page.
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      results: null,
-      redirectPage: false,
-    };
     this.handleResults = this.handleResults.bind(this);
   }
-  handleResults(results) {
-      this.setState({ results: results, redirectPage: true });
 
+  //Redirect page to /businesses with the search results
+  handleResults(newResults) {
+    this.props.history.push({
+      pathname: "/businesses",
+      // Pass results data to the search results page
+      state: { passResults: newResults },
+    });
   }
 
   render() {
-    if (this.state.redirectPage) {
-      return (
-        <Redirect
-          to={{
-            pathname: "/businesses",
-            state: { data: this.state.results },
-          }}
-        />
-      );
-    } else {
-      return (
-        <div className="App">
-          <Header />
-          <section className="home-container">
-            <h1>Corner Store</h1>
-            <SearchBusiness handleResults={this.handleResults} />
-          </section>
-          <span />
-          <SearchCategories handleResults={this.handleResults} />
-          <div></div>
-        </div>
-      );
-    }
+    return (
+      <div className="App">
+        <Header />
+        <section className="home-container">
+          <h1>Corner Store</h1>
+          {/* Show search bar and pass in redirect function as handleResults */}
+          <SearchBusiness handleResults={this.handleResults} />
+        </section>
+        <span />
+        {/* Show different categories and pass in redirect function as handleResults */}
+        <SearchCategories handleResults={this.handleResults} />
+        <div></div>
+      </div>
+    );
   }
 }
 

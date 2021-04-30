d327ab5493f4f2cd384bb307d36a8d7693a62765
hakroala <hakroala@bu.edu>
2021-04-30 15:27:14 -0400

    Added map to search result page

--- src/Pages/SearchResultsPage.js
+++ src/Pages/SearchResultsPage.js
@@ -1,11 +1,48 @@
 import React, { Component } from "react";
 import DisplayBusinesses from "../Components/DisplayBusinesses";
 import "../layoutMain.css";
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 //Description: The search results page displays the businesses from the home page and has
 //a search engine and the map of the business locations.
 
 class SearchResultsPage extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+ 
+    this.state = {
+      visible: false,
+      searchResults: this.props.location.state?.passResults,
+      places: [{lat: 47.49855629475769, lng: -122.14184416996333},
+        {latitude: 47.359423, longitude: -122.021071},
+        {latitude: 47.2052192687988, longitude: -121.988426208496},
+        {latitude: 47.6307081, longitude: -122.1434325},
+        {latitude: 47.3084488, longitude: -122.2140121},
+        {latitude: 47.5524695, longitude: -122.0425407}]
+    }
+
+    this.displayMarkers = this.displayMarkers.bind(this);
+  }
+
+  displayMarkers = () => {
+
+    // Retrieve data (if it exists) passed from home page and set to searchResults
+    const searchResults = this.props.location.state?.passResults;
+
+    return searchResults.map((business, index) => {
+      return business[7] && business[8] ? (
+        <Marker
+          key={index}
+          id={index}
+          position={{
+            lat: parseFloat(business[7]),
+            lng: parseFloat(business[8])
+          }}
+        />
+      ) : null
+    })
+  }
 
   render() {
     const itemSearched = this.props.location.state?.passItemSearched;
@@ -31,8 +68,25 @@ class SearchResultsPage extends Component {
           {/* Check if there is data available to display, if not, show no results */}
           {searchResults ? displaySearchResult : noResult}
         </div>
+        <div className="map-right">
+          <Map
+            google={this.props.google}
+            zoom={8}
+            style={ {
+              width: '30%',
+              height: '100%',
+            }}
+            initialCenter={{ lat: 42.407211, lng: -71.382439}}
+          >
+            {/* <Marker position={{ lat: 42.407211, lng: -71.382439}} /> */}
+            {this.displayMarkers()}
+          </Map>
+        </div>
       </section>
     );
   }
 }
-export default SearchResultsPage;
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBwzB1uzj0Wcs1JbdEBXFcWymGIG1it_6M'
+})(SearchResultsPage);
